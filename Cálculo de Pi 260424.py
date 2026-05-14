2c97e5c8bfe74f68261c0244c98a1efa2ea200ed
Manuel Diego Bezanilla <134969078+BestmanPi@users.noreply.github.com>
2026-05-14 19:10:50 +0200

    Refactor Pi calculation into a single block

--- Cálculo de Pi 260424.py
+++ Cálculo de Pi 260424.py
@@ -1,21 +1,17 @@
 from math import sqrt
 
+SCALE = 7 << 22
+RADIUS = SCALE
+R2 = RADIUS * RADIUS
+small = 0
+big = 0
 
-def pi():
-
-    SCALE = 7 << 22
-    RADIUS = SCALE
-    R2 = RADIUS * RADIUS
-    small = 0
-    big = 0
-
-    for i in range(RADIUS // 2):
-        y = i + 0.5
-        small += sqrt(R2 - y * y)
-        if small >= RADIUS:
-            small -= RADIUS
-            big += RADIUS
-    return (small / R2 - sqrt(3) / 8 + big / R2) * 12
+for i in range(RADIUS // 2):
+    y = i + 0.5
+    small += sqrt(R2 - y * y)
+    if small >= RADIUS:
+        small -= RADIUS
+        big += RADIUS
 
 
-print("Pi= ", pi())
+print("Pi= ", (small / R2 - sqrt(3) / 8 + big / R2) * 12)
